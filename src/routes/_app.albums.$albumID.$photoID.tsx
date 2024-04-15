31e2dc4ba15affabff932160ebf281dc93a74467
l.wisniewski <l.wisniewski@perfectone.pl>
2024-04-15 23:10:54 +0200

    feat: photo enlargement

--- src/routes/_app.albums.$albumID.$photoID.tsx
+++ src/routes/_app.albums.$albumID.$photoID.tsx
@@ -1,5 +1,69 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import "yet-another-react-lightbox/styles.css";
+import Lightbox from "yet-another-react-lightbox";
+import { useMemo } from 'react';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 export const Route = createFileRoute('/_app/albums/$albumID/$photoID')({
-  component: () => <div>Hello /albums/$albumID/photoID!</div>
+  component: AlbumsAlbumIDPhotoID,
+  loader: async ({ context, params }) => {
+    if (!context.auth.supabase) return []
+    const { data } = await context.auth.supabase.storage.from("pieski_photos")
+      .createSignedUrl(`${params.albumID}/${params.photoID}`, 20)
+    if (!data) return []
+    return [{ src: data.signedUrl }]
+  },
 })
+
+function AlbumsAlbumIDPhotoID() {
+  const params = Route.useParams()
+  const navigate = useNavigate()
+  const { photos } = Route.parentRoute.useLoaderData()
+  const currentPhotos = Route.useLoaderData()
+  const data = useMemo(() => {
+    const indexPhoto = photos.findIndex(p => p.path.includes(params.photoID))
+    const prevIndex = indexPhoto - 1 < 0 ? photos.length - 1 : indexPhoto - 1;
+    const nextIndex = photos.length - 1 < indexPhoto + 1 ? 0 : indexPhoto + 1;
+    const nextPhoto = photos[nextIndex].path.split("/")[1]
+    const prevPhoto = photos[prevIndex].path.split("/")[1]
+    return { prevPhoto, nextPhoto }
+  }, [photos, params.photoID])
+  console.log(data)
+  const handleClose = () => {
+    navigate({
+      to: `/albums/$albumID`,
+      params: {
+        albumID: params.albumID
+      }
+    })
+  }
+  return (
+    <Lightbox render={{
+      buttonPrev: () => (
+        <button className='yarl__button yarl__navigation_prev' onClick={() => {
+          navigate({
+            to: `/albums/$albumID/$photoID`,
+            params: {
+              albumID: params.albumID,
+              photoID: data.prevPhoto
+            }
+          })
+        }}><ChevronLeft /></button>
+      ),
+      buttonNext: () => (
+        <button className='yarl__button yarl__navigation_next' onClick={() => {
+          navigate({
+            to: `/albums/$albumID/$photoID`,
+            params: {
+              albumID: params.albumID,
+              photoID: data.nextPhoto
+            }
+          })
+        }}><ChevronRight /></button>
+      ),
+
+    }} open={true} close={handleClose} slides={currentPhotos} />
+  )
+}
--- src/routes/_app.albums.$albumID.tsx
+++ src/routes/_app.albums.$albumID.tsx
@@ -13,9 +13,26 @@ export const Route = createFileRoute('/_app/albums/$albumID')({
     if (!data) {
       return { photos: [] }
     }
-    const { data: urls } = await supabase.storage.from("pieski_photos")
-      .createSignedUrls(data.map(photo => photo.path), 20)
-    return { photos: urls?.map(u => ({ src: u.signedUrl, path: u.path })) ?? [] }
+    const urlsQueries = data.map(photo => new Promise<{ signedUrl: string, path: string }>((resolve, reject) => {
+      supabase.storage.from("pieski_photos").createSignedUrl(photo.path, 3600, {
+        transform: {
+          width: 500,
+          height: 350,
+          resize: "cover",
+          quality: 80
+        }
+      }).then(response => {
+        if (response.data) {
+          resolve({
+            signedUrl: response.data.signedUrl,
+            path: photo.path
+          })
+        }
+        reject('error')
+      })
+    }))
+    const urls = await Promise.all(urlsQueries)
+    return { photos: urls?.map(u => ({ src: u.signedUrl, path: u.path, width: 450, height: 350 })) ?? [] }
   },
   component: AlbumsAlbumID
 })
@@ -26,13 +43,11 @@ function AlbumsAlbumID() {
   const params = Route.useParams()
   return (
     <>
-      {/* @ts-expect-error missing types*/}
       <Gallery images={photos} onClick={(ev) => {
         navigate({
           to: "/albums/$albumID/$photoID",
           params: {
             albumID: params.albumID,
-            // @ts-expect-error missing types
             photoID: photos[ev].path?.split("/")[1]
           }
         })
